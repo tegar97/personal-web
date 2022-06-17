fbc42d11c857b3034653912621d387c631ba5b06
Irsyad A. Panjaitan <irsyadpanjaitan@gmail.com>
2022-06-17 19:20:00 +0700

    wip

--- components/Prose.js
+++ components/Prose.js
@@ -7,6 +7,6 @@ marked.setOptions({
 
 export default function Prose({ children }) {
     return (
-        <div className='prose prose-invert max-w-none' dangerouslySetInnerHTML={{ __html: marked(children) }}/>
+        <div className='prose prose-invert max-w-none prose-a:text-sky-400 prose-a:font-medium prose-a:shadow-undersky prose-a:no-underline' dangerouslySetInnerHTML={{ __html: marked(children) }}/>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
         extend: {
             boxShadow: (theme) => ({
                 undersky:
-                    theme('colors.gray.900') +
+                    theme('colors.slate.900') +
                     ' 0px -0.166667em 0px 0px inset, ' +
                     theme('colors.sky.800') +
                     ' 0px -0.333333em 0px 0px inset',
