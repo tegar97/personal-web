af5904f88220fe9f08122802ecfcc890cf055fc3
Irsyad A. Panjaitan <irsyadpanjaitan@gmail.com>
2022-09-13 12:27:57 +0700

    Delete pages directory

--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-import "../styles/app.css";
-export default function MyApp({ Component, pageProps }) {
-    // Use the layout defined at the page level, if available
-    const getLayout = Component.getLayout || ((page) => page);
-
-    return getLayout(<Component {...pageProps} />);
-}
