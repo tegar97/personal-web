4d02b903ccc9e03bbed38d8f6e85511a30a24faf
Irsyad A. Panjaitan <irsyadpanjaitan@gmail.com>
2022-10-29 00:57:23 +0700

    wip

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,12 @@
 import '../styles/globals.css';
-
+import { Analytics } from '@vercel/analytics/react';
 import Layout from '../components/Layout';
 
 export default function MyApp({ Component, pageProps }) {
     return (
         <Layout>
             <Component {...pageProps} />
+            <Analytics />
         </Layout>
     );
 }
